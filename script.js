af2f038ce7183451be8f74ffd596a4503e72fab2
xotamov <xotamovazizjon997@gmail.com>
2023-06-22 17:53:51 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let yourbalance 
+let limit = 0
+let you = 3
+do{
+    yourbalance = prompt('write your password')
+    if(yourbalance === '7777'){
+        break;
+    }
+    limit++
+}
+while(
+    limit!==3
+)
